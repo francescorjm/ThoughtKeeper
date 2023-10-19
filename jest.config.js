c0fd8dc88bc197457b3598204057fbd53f8d312d
francescorjm <francescorjm@gmail.com>
2023-10-19 06:51:21 -0400

    test: :white_check_mark: setup jest path mapping
    RESCATAAAAAAAAAA YA FUNCIONAN LAS PREUBAS YOLOLOLOLOLO

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,13 @@ module.exports = {
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
+  moduleNameMapper: {
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@middlewares/(.*)$': '<rootDir>/src/middlewares/$1',
+    '^@models/(.*)$': '<rootDir>/src/models/$1',
+    '^@routes/(.*)$': '<rootDir>/src/routes/$1'
+  },
   collectCoverage: true,
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.ts']
